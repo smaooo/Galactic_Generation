8371e8718885822eed8e4b2cc223dd60d30438a1
smaooo <31223968+smaooo@users.noreply.github.com>
2022-02-20 23:02:26 +0000

    testing shader custom function compatiblity with WebGL

--- Assets/Scripts/ProceduralMesh.cs
+++ Assets/Scripts/ProceduralMesh.cs
@@ -11,7 +11,22 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour
 {
-    [SerializeField, Range(1, 10)]
+
+    static MeshJobScheduleDelegate[] jobs =
+    {
+        MeshJob<SquareGrid, SingleStream>.ScheduleParallel,
+        MeshJob<SharedSquareGrid, SingleStream>.ScheduleParallel
+    };
+
+    public enum MeshType
+    {
+        SquareGrid, SharedSquareGrid
+    }
+
+    [SerializeField]
+    MeshType meshType;
+
+    [SerializeField, Range(1, 50)]
     private int resolution = 1;
     Mesh mesh;
     private void Awake()
@@ -39,9 +54,7 @@ public class ProceduralMesh : MonoBehaviour
         Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
         Mesh.MeshData meshData = meshDataArray[0];
 
-        MeshJob<SquareGrid, MultiStream>.ScheduleParallel(
-            mesh, meshData, resolution, default).Complete();
-
+        jobs[(int)meshType](mesh, meshData, resolution, default).Complete();
         Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
     }
 }
--- Assets/Scripts/ProceduralMeshes.cs
+++ Assets/Scripts/ProceduralMeshes.cs
@@ -37,6 +37,9 @@ namespace ProceduralMeshes
         }
 
     }
+
+    public delegate JobHandle MeshJobScheduleDelegate(
+        Mesh mesh, Mesh.MeshData meshData, int resolution, JobHandle dependency);
     public struct PVertex
     {
         public float3 position, normal;
@@ -264,6 +267,48 @@ namespace ProceduralMeshes.Generators
                 streams.SetTriangle(ti + 1, vi + int3(1, 2, 3));
             }
 
+        }
+    }
+
+    public struct SharedSquareGrid : IMeshGenerator
+    {
+        public int VertexCount => (Resolution + 1) * (Resolution + 1);
+
+        public int IndexCount => 6 * Resolution * Resolution;
+
+        public int JobLength => Resolution + 1;
+
+        public int Resolution { get; set; }
+        public Bounds Bounds => new Bounds(Vector3.zero, new Vector3(1f, 0f, 1f));
+
+        public void Execute<S>(int z, S streams) where S : struct, IMeshStreams
+        {
+            int vi = (Resolution + 1) * z, ti = 2 * Resolution * (z - 1);
+
+            PVertex vertex = new PVertex();
+            vertex.normal.y = 1f;
+            vertex.tangent.xw = float2(1f, -1f);
+
+            vertex.position.x = -0.5f;
+            vertex.position.z = (float)z / Resolution - 0.5f;
+            vertex.texCoord0.y = (float)z / Resolution;
+            streams.SetVertex(vi, vertex);
+
+            vi++;
+            for (int x = 1; x <= Resolution; x++, vi++, ti += 2)
+            {
+                vertex.position.x = (float)x / Resolution - 0.5f;
+                vertex.texCoord0.x = (float)x / Resolution;
+                streams.SetVertex(vi, vertex);
+
+                if (z > 0)
+                {
+                    streams.SetTriangle(
+                        ti + 0, vi + int3(-Resolution - 2, -1, -Resolution - 1));
+                    streams.SetTriangle(
+                        ti + 1, vi + int3(-Resolution - 1, -1, 0));
+                }
+            }
         }
     }
 }
