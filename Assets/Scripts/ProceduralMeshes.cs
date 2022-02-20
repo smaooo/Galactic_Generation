66d40975461165fa63771c63b0c57edb375804a7
smaooo <31223968+smaooo@users.noreply.github.com>
2022-02-20 20:50:23 +0000

    testing WebGL compatibility

--- Assets/Scripts/ProceduralMeshes.cs
+++ Assets/Scripts/ProceduralMeshes.cs
@@ -226,38 +226,43 @@ namespace ProceduralMeshes.Generators
 
         public int IndexCount => 6 * Resolution * Resolution;
 
-        public int JobLength => Resolution * Resolution;
+        public int JobLength => Resolution;
 
         public int Resolution { get; set; }
         public Bounds Bounds => new Bounds(Vector3.zero, new Vector3(1f, 0f, 1f));
 
-        public void Execute<S>(int i, S streams) where S : struct, IMeshStreams
+        public void Execute<S>(int z, S streams) where S : struct, IMeshStreams
         {
-            int vi = 4 * i, ti = 2 * i;
-            int z = i / Resolution;
-            int x = i - Resolution * z;
+            int vi = 4 * Resolution * z, ti = 2 * Resolution * z;
 
-            float4 coordinates = float4(x, x + 1f, z, z + 1f) / Resolution - 0.5f; 
-            var vertex = new PVertex();
-            vertex.normal.y = 1f;
-            vertex.tangent.xw = float2(1f, -1f);
+            for (int x = 0; x < Resolution; x++, vi += 4, ti += 2)
+            {
 
-            vertex.position.xz = coordinates.xz;
-            streams.SetVertex(vi + 0, vertex);
+                float2 xCoordinates = float2(x, x + 1f) / Resolution - 0.5f;
+                float2 zCoordinates = float2(z, z + 1f) / Resolution - 0.5f;
+                var vertex = new PVertex();
+                vertex.normal.y = 1f;
+                vertex.tangent.xw = float2(1f, -1f);
 
-            vertex.position.xz = coordinates.yz;
-            vertex.texCoord0 = float2(1f, 0f);
-            streams.SetVertex(vi + 1, vertex);
+                vertex.position.x = xCoordinates.x;
+                vertex.position.z = zCoordinates.x;
+                streams.SetVertex(vi + 0, vertex);
 
-            vertex.position.xz = coordinates.xw;
-            vertex.texCoord0 = float2(0f, 1f);
-            streams.SetVertex(vi + 2, vertex);
+                vertex.position.x = xCoordinates.y;
+                vertex.texCoord0 = float2(1f, 0f);
+                streams.SetVertex(vi + 1, vertex);
 
-            vertex.position.xz = coordinates.yw;
-            vertex.texCoord0 = 1f;
-            streams.SetVertex(vi + 3, vertex);
-            streams.SetTriangle(ti + 0, vi +int3(0, 2, 1));
-            streams.SetTriangle(ti + 1, vi + int3(1, 2, 3));
+                vertex.position.x = xCoordinates.x;
+                vertex.position.z = zCoordinates.y;
+                vertex.texCoord0 = float2(0f, 1f);
+                streams.SetVertex(vi + 2, vertex);
+
+                vertex.position.x = xCoordinates.y;
+                vertex.texCoord0 = 1f;
+                streams.SetVertex(vi + 3, vertex);
+                streams.SetTriangle(ti + 0, vi +int3(0, 2, 1));
+                streams.SetTriangle(ti + 1, vi + int3(1, 2, 3));
+            }
 
         }
     }
